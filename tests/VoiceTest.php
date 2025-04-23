c1948bc1a114502a5926fcadaaeb606260b759bb
taoran <taoran0796@163.com>
2025-04-23 09:38:50 +0800

    fix

--- tests/VoiceTest.php
+++ tests/VoiceTest.php
@@ -12,21 +12,21 @@ class VoiceTest extends \PHPUnit\Framework\TestCase
     {
         $voice = new Voice();
         $aliyunVoice = $voice->driver('aliyun', [
-            'appkey' => 'you appkey',
-            'token' => 'you token',
+            'appkey' => 'Nr656ZMxQxhbxl5h',
+            'token' => '7de20354eb9f4bec8501d0cca53a20cd',
         ]);
         $this->assertInstanceOf(DriverInterface::class, $aliyunVoice);
 
-        $response = $aliyunVoice->setText('今天是周一，天气挺好的。')
-            ->setVoice('xiaoyun')
+        $response = $aliyunVoice->setText('你好啊，你好啊，你好啊，你好啊，你好啊，你好啊，你好啊，')
+            ->setVoice('aijia')
             ->setFormat('mp3')
             ->tts();
 
         $this->assertInstanceOf(ResponseInterface::class, $response);
 
         //save file
-        /*if ($response->getStatusCode() == 200 && in_array('audio/mpeg', $response->getHeader('Content-Type'))) {
+        if ($response->getStatusCode() == 200 && in_array('audio/mpeg', $response->getHeader('Content-Type'))) {
             file_put_contents('./tmp.mp3', $response->getBody());
-        }*/
+        }
     }
 }
